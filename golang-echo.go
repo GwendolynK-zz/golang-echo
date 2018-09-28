727a3567d6ce5d5ff340976e06ecfa7e3a92355b
WestleyK <westley@sylabs.io>
2018-09-27 19:30:39 -0700

    v-1.0.3 updated code, new functions

--- golang-echo.go
+++ golang-echo.go
@@ -1,8 +1,8 @@
 // created by: WestleyK
 // email: westleyk@nym.hush.com
 // https://github.com/WestleyK/golang-echo
-// date: Sep 19, 2018
-// version-1.0.2
+// date: Sep 27, 2018
+// version-1.0.3
 //
 // MIT License
 //
@@ -34,16 +34,20 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
 )
 
 var (
-    VERSION string = "version-1.0.2"
-    DATE string = "Date: Sep 19, 2018"
+    VERSION string = "version-1.0.3"
+    DATE string = "Date: Sep 27, 2018"
 
     OPTION string = ""
     OPTION2 string = ""
+    OPTION3 string = ""
     SCRIPT_NAME string = ""
     noNewLine int = 0
+    NO_RESET int = 0
+    OPTION_E int = 0
     color string = ""
     VAR string = ""
     MSG string = ""
@@ -71,17 +75,36 @@ func info_print() {
 }
 
 func help_menu() {
-    fmt.Print("usage: $ ", SCRIPT_NAME, " [OPTION] [OPTION]\n")
-    fmt.Print("      -h | -help | --help (print usage menu)\n")
-    fmt.Print("      -n [MESSAGE] (no new line)\n")
-    fmt.Print("      -red [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -yello [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -green [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -blue [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -white [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -teal [MESSAGE] (print message in givin color)\n")
-    fmt.Print("      -i | -info | --info (print script info)\n")
-    fmt.Print("      -v | -version | --version (print script version)\n")
+    fmt.Print("usage: $ ", SCRIPT_NAME, " [OPTION] [OPTION] [OPTION] [MESSAGE]\n")
+    fmt.Print("      -h | -help | --help\n")
+    fmt.Print("            print usage menu)\n")
+    fmt.Print("      -n [MESSAGE]\n")
+    fmt.Print("            no new line\n")
+    fmt.Print("      -r [OPTION] [OPTION] [MESSAGE]\n")
+    fmt.Print("            dont reset the color.\n")
+    fmt.Print("      -s [OPTION] [OPTION] [MESSAGE]\n")
+    fmt.Print("            resets the color output.\n")
+    fmt.Print("      -e [OPTION] [MESSAGE]\n")
+    fmt.Print("            use the \\ options,\n")
+    fmt.Print("            eg. $ ", SCRIPT_NAME, " -e \"your message\"\n")
+    fmt.Print("      -f | -force [MESSAGE]\n")
+    fmt.Print("            force print message, even if its -help\n")
+    fmt.Print("      -red [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -yello [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -green [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -blue [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -white [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -teal [MESSAGE]\n")
+    fmt.Print("            print message in givin color\n")
+    fmt.Print("      -i | -info | --info\n")
+    fmt.Print("            print script info\n")
+    fmt.Print("      -v | -version | --version\n")
+    fmt.Print("            print script version)\n")
     fmt.Print("Source code: https://github.com/WestleyK/golang-echo\n")
     os.Exit(0)
 }
@@ -89,7 +112,7 @@ func help_menu() {
 func fail(err1 string, err2 string) {
     if err2 == "help" {
         fmt.Print(red, "ERROR: ", colorReset, err1)
-        fmt.Print("Try: $ ", SCRIPT_NAME, "-help  (for help menu)\n")
+        fmt.Print("Try: $ ", SCRIPT_NAME, " -help  (for help menu)\n")
         os.Exit(1)
     }
     fmt.Print(red, "ERROR: ", colorReset, err1, err2)
@@ -98,12 +121,28 @@ func fail(err1 string, err2 string) {
     os.Exit(1)
 }
 
+func reset_color() {
+    fmt.Print(colorReset)
+    os.Exit(0)
+}
+
 func check_args(OPTION string) {
     if OPTION == "-h" || OPTION == "-help" || OPTION == "--help" {
         help_menu()
         return
     } else if OPTION == "-n" {
         noNewLine = 1
+    } else if OPTION == "-e" {
+        OPTION_E = 1
+        return
+    } else if OPTION == "-r" {
+        NO_RESET = 1
+        return
+    } else if OPTION == "-s" {
+        reset_color()
+        return
+    } else if OPTION == "-f" || OPTION == "-force" {
+        return
     } else if OPTION == "-red" {
         color = "\x1b[31m"
         return
@@ -128,10 +167,16 @@ func check_args(OPTION string) {
     } else if OPTION == "-v" || OPTION == "-version" || OPTION == "--version" {
         version_print()
         return
+    } else if strings.Contains(OPTION, "-") {
+        fail("option not found: ", OPTION)
+        return
     }
 }
 
 func print_message(color string, MSG string) {
+    if OPTION_E == 1 {
+        fail("-e option is comming soon!\n", "option not avalible.")
+    }
     if len(color) == 0 {
         fmt.Print(MSG)
         if noNewLine != 1 {
@@ -140,7 +185,10 @@ func print_message(color string, MSG string) {
         os.Exit(0)
     }
 
-    fmt.Print(color, MSG, colorReset)
+    fmt.Print(color, MSG)
+    if NO_RESET != 1 {
+        fmt.Print(colorReset)
+    }
     
     if noNewLine != 1 {
         fmt.Print("\n")
@@ -171,16 +219,26 @@ func three_args() {
     print_message(color, MSG)
 }
 
+func four_args() {
+    OPTION = os.Args[1]
+    OPTION2 = os.Args[2]
+    OPTION3 = os.Args[3]
+    MSG = os.Args[4]
+    check_args(OPTION)
+    check_args(OPTION2)
+    check_args(OPTION3)
+    print_message(color, MSG)
+}
+
 func main() {
     SCRIPT_NAME = os.Args[0]
 
     if len(os.Args[1:]) == 0 {
-        fail("no arguments!\n", "need at least one argument")
+        fmt.Print("\n")
+        //fail("no arguments!\n", "need at least one argument")
     }
 
-
     arg_len := len(os.Args)
-
 
     if arg_len == 2 {
         one_args()
@@ -188,14 +246,14 @@ func main() {
         two_args()
     } else if arg_len == 4 {
     	three_args()
-    } else if arg_len >= 5 {
+    } else if arg_len == 5 {
+        four_args()
+    } else if arg_len >= 6 {
         fail("too many arguments!\n", "help")
     }
 
-
     return
 }
-
 
 
 
